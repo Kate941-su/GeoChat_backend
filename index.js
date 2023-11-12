ca1e883f77f0ce11c0eaf60dc9da930b26221176
kitaya <kkitaya@nzigen.com>
2023-11-12 11:39:08 +0900

    first : first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const http = require('http');
+const { createServer } = require("http");
+const  server = createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const port = 3000
+
+app.route("/").get((req, res)=>{
+    res.json("Hey thre welcome again on dev stack channel !!")
+});
+
+/*
+@param : connection is predefined.
+*/
+io.on("connection", (socket) => {
+    console.log("【JS】 connect to frontend")
+});
+
+
+server.listen(port, ()=> {
+    console.log(`listen on * : ${port}`);
+})
